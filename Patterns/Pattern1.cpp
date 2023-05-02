b0fdc31f6a260435db8eb72e7b4da4394d85b32e
Somya Shirotriya <114052564+sshirotriya@users.noreply.github.com>
2023-05-02 16:55:19 +0530

    Update Pattern1.cpp

--- Patterns/Pattern1.cpp
+++ Patterns/Pattern1.cpp
@@ -33,3 +33,23 @@ int main()
 }
 
 --------------------------------------------------
+     
+     
+int main()
+{
+     int i, j,n;
+     cin>>n;
+     for(i = 0 ; i < n ; i++)
+     {
+          for(j = 0 ; j < n ; j++)
+          {
+               if(i+j >= n+1)
+               {
+                    cout<<"*";
+               }
+               cout<<endl;
+          }
+
+    return 0;
+}
+
