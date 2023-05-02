91e30273f639fb4a863225d8136888ab466acf00
Somya Shirotriya <114052564+sshirotriya@users.noreply.github.com>
2023-05-03 02:20:25 +0530

    Update Pattern4.cpp

--- Patterns/Pattern4.cpp
+++ Patterns/Pattern4.cpp
@@ -13,14 +13,14 @@ using namespace std;
 
 int main()
 {
-    int n, count =0;
+    int n, count = 1;
     cin>>n;
     for(int i = 1 ; i <= n ; i++)
     {
         for(int j = 1 ; j <= i ; j++)
         {
-            count++;
-            cout<<count<< " ";
+           cout<<count<< " ";
+           count++;
         }
         cout<<endl;
     }
