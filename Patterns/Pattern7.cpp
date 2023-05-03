097b098f3190eeeeabec0f53f4eb72c8d2b99ee6
Somya Shirotriya <114052564+sshirotriya@users.noreply.github.com>
2023-05-03 15:54:01 +0530

    Pattern7.cpp

--- Patterns/Pattern7.cpp
+++ Patterns/Pattern7.cpp
@@ -3,8 +3,7 @@
 1 2 3
 1 2 
 1 
-  
--------------------------------------------------  
+ 
 #include <iostream>
 
 using namespace std;
@@ -24,3 +23,62 @@ int main()
 
     return 0;
 }
+
+-----------------------------------------------------
+  
+1
+1 2
+1 2 3
+1 2 3 4 
+1 2 3 4 5
+  
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+     for(int i = 1; i<=n;i++)
+     {
+         for(int j = 1; j<=i;j++)
+         {
+             cout<<j<<" ";
+         }
+         cout<<endl;
+     }
+
+    return 0;
+}
+
+-----------------------------------------------------
+5
+4 4
+3 3 3
+2 2 2 2 
+1 1 1 1 1
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    
+    for(int i= n; i>=1; i-- )
+    {
+        for(int j =1; j<=n+1-i;j++)
+        {
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
+
+
