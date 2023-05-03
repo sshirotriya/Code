3f6c93243694e87385b22b3b46519d24d1d0d1cb
Somya Shirotriya <114052564+sshirotriya@users.noreply.github.com>
2023-05-03 16:58:23 +0530

    Pattern5.cpp

--- Patterns/Pattern5.cpp
+++ Patterns/Pattern5.cpp
@@ -1,54 +1,91 @@
-     #
-    ##
-   ###
-  ####
- #####
-######
+// Butterfly Pattern
+
+*             *
+* *         * * 
+* * *     * * *
+* * * * * * * *
+* * * * * * * *
+* * *     * * *
+* *         * *
+*             *
 
 
-   #include <iostream> 
-   using namespace std;
-   int main() 
-   {
-       int n,i,j,k; 
-       cin>>n;
-       
-       for( i = 1 ; i <= n ; i++) 
-       { 
-           for( j = 1; j <= n-i ; j++) 
-           {
-                cout<<" ";
-           } 
-           for( k = 1 ; k <= i ; k++)
-           {
-               cout<<"#";
-           }
-           cout<<endl; 
-          
-        }
-        return 0; 
-    }
-     
- ------------------------------------------------- 
-      
 #include <iostream>
+
+using namespace std;
+
+int
+main ()
+{
+  int n, count = 1;
+  cin >> n;
+  for (int i = 1 ; i <= n ; i++)
+    {
+      for (int j = 1 ; j <= i ; j++)
+	{
+        cout<<"*";
+	}
+	int space = 2*n-2*i;
+	for(int j = 1 ; j <= space ; j++)
+	{
+	    cout<<" ";
+	}
+	 for (int j = 1 ; j <= i ; j++)
+	{
+        cout<<"*";
+	}
+      cout << endl;
+    }
+    
+    for (int i = n ; i >= 1 ; i--)
+    {
+      for (int j = 1 ; j <= i ; j++)
+	{
+        cout<<"*";
+	}
+	int space = 2*n-2*i;
+	for(int j = 1 ; j <= space ; j++)
+	{
+	    cout<<" ";
+	}
+	 for (int j = 1 ; j <= i ; j++)
+	{
+        cout<<"*";
+	}
+      cout << endl;
+    }
+
+
+  return 0;
+}
+
+-------------------------------------------------
+
+        * * * * *
+      * * * * *
+    * * * * *
+  * * * * *
+* * * * *
+
+#include <iostream>
+
 using namespace std;
 
 int main()
 {
     int n;
     cin>>n;
-    for(int i = 1 ; i <= n ; i++)
+    
+    for(int i = 1; i <= n; i++ )
     {
-        for(int j = 1 ; j <= n ; j++)
+        for(int j =1; j<=n-i;j++)
         {
-           if(j <= n-i)
-           {
-               cout<<" ";
-           }
-           else{
-               cout<<"*";
-           }
+            cout<<" ";
+            
+        }
+        for(int k = 1 ; k <= n ; k++)
+        {
+            cout<<"*";
         }
         cout<<endl;
     }
