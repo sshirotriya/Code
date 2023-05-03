47fb63f2689eaa98cebaf5f0956a141391329466
Somya Shirotriya <114052564+sshirotriya@users.noreply.github.com>
2023-05-03 16:55:51 +0530

    Pattern4.cpp

--- Patterns/Pattern4.cpp
+++ Patterns/Pattern4.cpp
@@ -1,11 +1,8 @@
-Floyd's Triangle
-
 1
-2 3
-4 5 6
-7 8 9 10
-  
--------------------------
+0 1
+1 0 1
+0 1 0 1
+1 0 1 0 1
 
 #include <iostream>
 
@@ -13,17 +10,20 @@ using namespace std;
 
 int main()
 {
-    int n, count = 1;
+    int n;
     cin>>n;
-    for(int i = 1 ; i <= n ; i++)
-    {
-        for(int j = 1 ; j <= i ; j++)
-        {
-           cout<<count<< " ";
-           count++;
-        }
-        cout<<endl;
-    }
+   for(int i = 1 ; i <= n; i++)
+   {
+       for(int j = 1 ; j <= i ; j++)
+       {
+           if((i+j)%2 == 0)
+           {
+               cout<<"1"<<" ";
+           }
+           else cout<<"0"<<" ";
+       }
+       cout<<endl;
+   }
 
     return 0;
 }
