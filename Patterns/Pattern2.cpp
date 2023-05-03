d7fcf68172c3ae3af91b891c4838bdc6c815d0e4
Somya Shirotriya <114052564+sshirotriya@users.noreply.github.com>
2023-05-03 16:44:46 +0530

    Pattern2.cpp

--- Patterns/Pattern2.cpp
+++ Patterns/Pattern2.cpp
@@ -23,3 +23,64 @@ int main()
 
     return 0;
 }
+
+
+------------------------------------------------
+    
+     #
+    ##
+   ###
+  ####
+ #####
+######
+
+
+   #include <iostream> 
+   using namespace std;
+   int main() 
+   {
+       int n,i,j,k; 
+       cin>>n;
+       
+       for( i = 1 ; i <= n ; i++) 
+       { 
+           for( j = 1; j <= n-i ; j++) 
+           {
+                cout<<" ";
+           } 
+           for( k = 1 ; k <= i ; k++)
+           {
+               cout<<"#";
+           }
+           cout<<endl; 
+          
+        }
+        return 0; 
+    }
+     
+   //OR
+      
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i = 1 ; i <= n ; i++)
+    {
+        for(int j = 1 ; j <= n ; j++)
+        {
+           if(j <= n-i)
+           {
+               cout<<" ";
+           }
+           else{
+               cout<<"*";
+           }
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
